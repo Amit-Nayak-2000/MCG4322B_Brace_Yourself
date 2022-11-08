5c13ee5ecc115c9fa7fed6bb3cc9c7119010952b
JenniferTrac <jtrac019@uottawa.ca>
2022-11-08 11:03:44 -0500

    force calcs for superior stress

--- StressCalculations.m
+++ StressCalculations.m
@@ -6,6 +6,9 @@ function [] = SuperiorStress()
 % description
 
 %% Symbolic Variables
+% constants
+g = 9.81;
+
 % longitudinal forces
 syms F_tt F_tn F_scomxlong F_scomylong F_saxlong F_saylong
 syms F_spxlong F_spylong F_spring1
@@ -16,7 +19,14 @@ syms tau_s1 tau_s2 tau_s3 sigma_srupture
 
 %% longitudinal force calculations
 F_tt = -Superior.F_t(1)*sind(Superior.theta) + Superior.F_t(2)*cosd(Superior.theta);
-
+F_tn = Superior.F_t(1)*cosd(Superior.theta) + Superior.F_t(2)*sind(Superior.theta);
+F_scomxlong = Superior.m*Superior.a(1)*cosd(Superior.theta) + Superior.m*Superior.a(2)*sind(Superior.theta) - Superior.m*g*sind(Superior.theta); 
+F_scomylong = -Superior.m*Superior.a(1)*sind(Superior.theta) + Superior.m*Superior.a(2)*cosd(Superior.theta) - Superior.m*g*cosd(Superior.theta);
+F_saxlong = Superior.F_sa(1)*cosd(Superior.theta) + Superior.F_sa(2)*sind(Superior.theta);
+F_saylong = -Superior.F_sa(1)*sind(Superior.theta) + Superior.F_sa(2)*cosd(Superior.theta);
+F_spxlong = Superior.F_sp(1)*cosd(Superior.theta) + Superior.F_sp(2)*sind(Superior.theta);
+F_spylong = -Superior.F_sp(1)*sind(Superior.theta) + Superior.F_sp(2)*cosd(Superior.theta);
+F_spring1 = TorsionalSpring.Torque(3)*0.4*Superior.L;
 
 %% stress calculations
 
