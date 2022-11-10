d0b4eb94bf53944e2198ff12ea0d37e1c98d3fac
lynnengo <71230404+lynnengo@users.noreply.github.com>
2022-11-09 19:15:03 -0500

    Update StressCalculations.m

--- StressCalculations.m
+++ StressCalculations.m
@@ -28,7 +28,21 @@ F_tt = -Superior.F_t(1)*sind(Superior.theta) + Superior.F_t(2)*cosd(Superior.the
 
 end
 function [] = InferiorStress()
-%% stresses
+% calculates stresses related to inferior link
+% inputs: 
+% output: 
+
+% description
+
+%% Symbolic Variables
+% constants
+g = 9.81;
+
+% longitudinal forces
+syms F_ct F_cn F_icomxlong F_icomylong F_iaxlong F_iaylong
+syms F_ipxlong F_ipylong F_spring2
+
+% stresses
 syms sigma_ix sigma_i1y sigma_i2y sigma_i3y sigma_ibend1 sigma_ibend2
 syms tau_i1 tau_i2 tau_i3 sigma_irupture
 
@@ -41,7 +55,7 @@ F_iaxlong = Inferior.F_ia(1)*cosd(Inferior.theta) + Inferior.F_ia(2)*sind(Inferi
 F_iaylong = -Inferior.F_ia(1)*sind(Inferior.theta) + Inferior.F_ia(2)*cosd(Inferior.theta);
 F_ipxlong = Inferior.F_ip(1)*cosd(Inferior.theta) + Inferior.F_ip(2)*sind(Inferior.theta);
 F_ipylong = -Inferior.F_ip(1)*sind(Inferior.theta) + Inferior.F_isp(2)*cosd(Inferior.theta);
-F_spring2 = -TorsionalSpring.Torque(3)*0.4*Inferior.L; %i think its the 0.4 of the superior link right? check
+F_spring2 = -TorsionalSpring.Torque(3)*0.4*Inferior.L; %CHECK
 
 end
 function [] = AnteriorStress()
