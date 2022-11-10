675b65f95b9b74d300969c27f7481373df4b9b89
lynnengo <71230404+lynnengo@users.noreply.github.com>
2022-11-09 19:11:53 -0500

    Update StressCalculations.m

--- StressCalculations.m
+++ StressCalculations.m
@@ -28,6 +28,20 @@ F_tt = -Superior.F_t(1)*sind(Superior.theta) + Superior.F_t(2)*cosd(Superior.the
 
 end
 function [] = InferiorStress()
+%% stresses
+syms sigma_ix sigma_i1y sigma_i2y sigma_i3y sigma_ibend1 sigma_ibend2
+syms tau_i1 tau_i2 tau_i3 sigma_irupture
+
+%% longitudinal force calculations
+F_ct = -Inferior.F_c(1)*sind(Inferior.theta) + Inferior.F_c(2)*cosd(Inferior.theta);
+F_cn = Inferior.F_c(1)*cosd(Inferior.theta) + Inferior.F_c(2)*sind(Inferior.theta);
+F_icomxlong = Inferior.m*Inferior.a(1)*cosd(Inferior.theta) + Inferior.m*Inferior.a(2)*sind(Inferior.theta) - Inferior.m*g*sind(Inferior.theta); 
+F_icomylong = -Inferior.m*Inferior.a(1)*sind(Inferior.theta) + Inferior.m*Inferior.a(2)*cosd(Inferior.theta) - Inferior.m*g*cosd(Inferior.theta);
+F_iaxlong = Inferior.F_ia(1)*cosd(Inferior.theta) + Inferior.F_ia(2)*sind(Inferior.theta);
+F_iaylong = -Inferior.F_ia(1)*sind(Inferior.theta) + Inferior.F_ia(2)*cosd(Inferior.theta);
+F_ipxlong = Inferior.F_ip(1)*cosd(Inferior.theta) + Inferior.F_ip(2)*sind(Inferior.theta);
+F_ipylong = -Inferior.F_ip(1)*sind(Inferior.theta) + Inferior.F_isp(2)*cosd(Inferior.theta);
+F_spring2 = -TorsionalSpring.Torque(3)*0.4*Inferior.L; %i think its the 0.4 of the superior link right? check
 
 end
 function [] = AnteriorStress()
