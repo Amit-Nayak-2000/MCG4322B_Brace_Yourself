cc731a9a5c0310dedc5164eae439577393276ed2
lynnengo <71230404+lynnengo@users.noreply.github.com>
2022-11-14 10:13:50 -0500

    fixed shear

--- StressCalculations.m
+++ StressCalculations.m
@@ -83,14 +83,14 @@ end
 
 %% Inferior Link
 
+% Symbols
 % longitudinal forces
 syms F_ct F_cn F_icomxlong F_icomylong F_iaxlong F_iaylong
 syms F_ipxlong F_ipylong F_spring2
 
 % stresses
 syms sigma_ix sigma_i1y sigma_i2y sigma_i3y sigma_ibend1 sigma_ibend2
-syms tau_i1 tau_i2 tau_i3 sigma_irupture
-syms moment_i1 moment_i2 shearforce_i1 shearforce_i2 shearforce_i3
+syms tau_i1 tau_i2 tau_i3 sigma_irupture moment_i1 moment_i2 
 
 % longitudinal force calculations
 F_ct = -Inferior.F_c(1)*sind(Inferior.theta) + Inferior.F_c(2)*cosd(Inferior.theta);
@@ -101,7 +101,7 @@ F_iaxlong = Inferior.F_ia(1)*cosd(Inferior.theta) + Inferior.F_ia(2)*sind(Inferi
 F_iaylong = -Inferior.F_ia(1)*sind(Inferior.theta) + Inferior.F_ia(2)*cosd(Inferior.theta);
 F_ipxlong = Inferior.F_ip(1)*cosd(Inferior.theta) + Inferior.F_ip(2)*sind(Inferior.theta);
 F_ipylong = -Inferior.F_ip(1)*sind(Inferior.theta) + Inferior.F_isp(2)*cosd(Inferior.theta);
-F_spring2 = -TorsionalSpring.Torque(3)*0.4*Inferior.L; %CHECK
+F_spring2 = -TorsionalSpring.Torque(3)*0.4*Inferior.L;
 
 % stress calculations
 % along x long
@@ -114,10 +114,10 @@ end
 % along y long
 if abs(F_ct + F_icomylong)>abs(F_iaylong + F_ipylong + F_spring2)
     sigma_i1y = (F_ct + F_icomylong)/(Inferior.B1*Inferior.T);
-    %add rupture
+    %add rupture sigma_irupture = 
 else
     sigma_i1y = (F_iaylong + F_ipylong + F_spring2)/(Inferior.B1*Inferior.T);
-    %add rupture
+    %add rupture sigma_irupture = 
 end
 
 if abs(F_ct)>abs(F_icomylong + F_iaylong + F_ipylong + F_spring2)
@@ -129,22 +129,11 @@ else
 end
 
 % bending
-moment_i1 = (Inferior.B3/2)*F_ct - (Inferior.B3/2 - Inferior.B2/2)*(F_icomylong + F_iaylong + F_ipylong + F_spring2);
-% add I*alpha
-moment_i2 = Inferior.I*Inferior.alpha(3) - (Inferior.L/2)*F_iaylong - (Inferior.H4 - Inferior.com(2) - Inferior.H1/2)*F_iaxlong + (Inferior.L/2)*F_ipylong - (Inferior.H4 - Inferior.com(2) - Inferior.H1/2)*F_ipxlong + ((Inferior.L/2)-0.4*Inferior.L)*F_spring2;
-% add F_cn (+)
+moment_i1 = Inferior.I*Inferior.alpha(3) + (Inferior.B3/2)*F_ct - (Inferior.B3/2 - Inferior.B2/2)*(F_icomylong + F_iaylong + F_ipylong + F_spring2);
+moment_i2 = Inferior.I*Inferior.alpha(3) - (Inferior.L/2)*F_iaylong - (Inferior.H4 - Inferior.com(2) - Inferior.H1/2)*F_iaxlong + (Inferior.L/2)*F_ipylong - (Inferior.H4 - Inferior.com(2) - Inferior.H1/2)*F_ipxlong + ((Inferior.L/2)-0.4*Inferior.L)*F_spring2 + Inferior.com(2)*F_cn;
 
-sigma_ibend1 = -moment_i1*((Inferior.H4-Inferior.H3)/2) / ((Inferior.B3*(Inferior.H4-Inferior.H3)^3)/12)
+sigma_ibend1 = -moment_i1*((Inferior.H4-Inferior.H3)/2) / ((Inferior.B3*(Inferior.H4-Inferior.H3)^3)/12);
 sigma_ibend2 = %idk what y and I is for this shape
-
-% shear
-shearforce_i1 = F_iaxlong + F_ipxlong; 
-shearforce_i2 = F_icomxlong;
-shearforce_i3 = F_cn;
-% slight confusion, i think im supposed to keep adding...
-tau_i1 = shear_i1/(Inferior.B1*Inferior.T); %should i use B1 here or should we use 
-tau_i2 = shear_i2/(Inferior.B2*Inferior.T);
-tau_i3 = shear_i3/(Inferior.B3*Inferior.T);
 
 % shear
 if abs(F_cn + F_icomxlong) > abs(F_iaxlong + F_ipxlong)
